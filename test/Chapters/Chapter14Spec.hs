e4fdef041f9cd6db88e661da6b1d9a30501ac04a
Juan Pablo Royo <juanpablo.royo@gmail.com>
2018-06-29 17:15:43 +0200

    adding chapter 14

--- test/Chapters/Chapter14Spec.hs
+++ test/Chapters/Chapter14Spec.hs
@@ -2,10 +2,13 @@
 
 module Chapters.Chapter14Spec (spec) where
 
+import           Chapters.Chapter9
 import           Data.List             (sort)
 import           Test.Hspec
 import           Test.Hspec.QuickCheck
 import           Test.QuickCheck
+
+data Fool = Fulse | Frue deriving (Eq, Show)
 
 half :: Fractional a => a -> a
 half x = x / 2
@@ -16,6 +19,18 @@ listOrdered xs =
     where go _ status@(_, False) = status
           go y (Nothing, t)      = (Just y, t)
           go y (Just x, _)       = (Just y, x >= y)
+
+twice :: (a -> a) -> a -> a
+twice f = f . f
+
+fourTimes :: (a -> a) -> a -> a
+fourTimes = twice . twice
+
+foolEqualsProb :: Gen Fool
+foolEqualsProb = oneof [return Fulse, return Frue]
+
+foolOneOfTreeProb :: Gen Fool
+foolOneOfTreeProb = frequency [(2, return Fulse), (3, return Frue)]
 
 nonZero :: Gen Integer
 nonZero = fmap getNonZero arbitrary
@@ -65,3 +80,12 @@ spec = do
     describe "read show roundtrip" $
         modifyMaxSize (const 1000) $ prop "(read (show x)) == x" $
             \(x :: String) -> (read (show x)) == x
+    describe "twice and fourTimes" $ do
+        modifyMaxSize (const 1000) $ prop "(capitalize x == twice capitalize x)" $
+            \(x :: String) -> capitalize x == twice capitalize x
+        modifyMaxSize (const 1000) $ prop "(capitalize x == fourTimes capitalize x)" $
+            \(x :: String) -> capitalize x == fourTimes capitalize x
+        modifyMaxSize (const 1000) $ prop "(sort x == twice sort x)" $
+            \(x :: [Int]) -> sort x == twice sort x
+        modifyMaxSize (const 1000) $ prop "(sort x == fourTimes sort x)" $
+            \(x :: [Int]) -> sort x == fourTimes sort x
