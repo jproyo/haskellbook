ec8b422a64997d363f2c8024baae8092dc62d923
Juan Pablo Royo <juanpablo.royo@gmail.com>
2018-10-05 12:45:52 +0200

    adding

--- src/Chapters/Chapter25.hs
+++ src/Chapters/Chapter25.hs
@@ -13,3 +13,13 @@ instance (Applicative f, Applicative g) => Applicative (Compose f g) where
 
     (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
     (Compose f) <*> (Compose a) = Compose $ (<*>) <$> f <*> a
+
+instance (Foldable f, Foldable g) => Foldable (Compose f g) where
+    foldMap :: Monoid m => (a -> m) -> Compose f g a -> m
+    foldMap f (Compose fga) = (foldMap . foldMap) f fga
+
+
+
+instance (Traversable f, Traversable g) => Traversable (Compose f g) where
+    traverse :: Applicative h => (a -> h b) -> Compose f g a -> h (Compose f g b)
+    traverse f (Compose fga) = Compose <$> (traverse . traverse) f fga
